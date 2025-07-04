73939b7b26a67bb9f42adb28c35e47907848c22a
Saurabh Misra <misra.saurabh1@gmail.com>
2025-07-03 21:21:50 -0700

    test with bubble_sort

---
+++ albumentations/core/bubble_sort.py
@@ -0,0 +1,9 @@
+from typing import Union, List
+def sorter(arr: Union[List[int],List[float]]) -> Union[List[int],List[float]]:
+    for i in range(len(arr)):
+        for j in range(len(arr) - 1):
+            if arr[j] > arr[j + 1]:
+                temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+    return arr
